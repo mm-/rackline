2160794c9ab110cbcdc9030d379d1c62edf8ed25
MM <masen@sutoroot.com>
2012-04-27 16:14:28 -0500

    Added the --id_only option to spit out servers with just ID
    Implimented help_now for the help message on username/api_key testing
    
    Added the byname_idonly method to the find ServerFind class
    
    Misc cleanup and commenting

--- lib/rackline/find.rb
+++ lib/rackline/find.rb
@@ -21,5 +21,17 @@ module Rackline
       end
     end
   end
+
+  def byname_idonly
+    cs=$cs
+    cs.servers_detail.each do |s|
+      name = s[:name].to_s
+      id = s[:id].to_s
+      tag = @name 
+      if name.match(/#{tag}/)
+        puts id
+      end
+    end
+  end
  end
 end
