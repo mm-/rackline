cff7f54746e239f7b22f41c6f0c659124b5659f7
Masen Marshall <masen@sutoroot.com>
2012-04-29 19:02:47 -0500

    Big functionality update
    Added --full_info
    
    This shows a big printout of all the info for every server found by name
    It should all line up correctly in collums thanks to some printf magic
    
    Also fixed some misc bugs
    
    Next update is going to enable the config file

--- lib/rackline/find.rb
+++ lib/rackline/find.rb
@@ -17,7 +17,35 @@ module Rackline
       id = s[:id].to_s
       tag = @name 
       if name.match(/#{tag}/)
-        puts name + " -- " + id
+       puts name + " -- " + id
+      end
+    end
+  end
+
+  def byname_fullinfo
+    cs=$cs
+    cs.servers_detail.each do |s|
+      name = s[:name].to_s
+      status = s[:status].to_s
+      id = s[:id].to_s
+      pub_ip = s[:addresses][:public].to_s.gsub(/([\[\]\"])/, '')
+      priv_ip = s[:addresses][:private].to_s.gsub(/([\[\]\"])/, '')      
+      metadata = s[:metadata].to_s
+      host_id = s[:hostId].to_s
+      image_id = s[:imageId].to_s
+      progress = s[:progress].to_s
+      tag = @name 
+      if name.match(/#{tag}/)
+        puts 
+        printf "%-20s %s\n", "Name", name
+        puts '=' * 50
+        printf "%-20s %s\n", "Status", status + " (" + progress + ")"
+        printf "%-20s %s\n", "Metadata", metadata        
+        printf "%-20s %s\n", "ID", id        
+        printf "%-20s %s\n", "Host ID", host_id
+        printf "%-20s %s\n", "Image ID", image_id 
+        printf "%-20s %s\n", "Public IP", pub_ip
+        printf "%-20s %s\n", "Private IP", priv_ip
       end
     end
   end
